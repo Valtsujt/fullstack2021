233c94790416f15ca688cc4cdb276f614896c34b
Valtteri Tuominen <valtteri.tuominen@aalto.fi>
2021-07-09 16:26:52 +0300

    fixed blogform deleton and removed blogform.test.js thats not used

---
+++ osa5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+const BlogForm = ({ createBlog }) => {
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [url, setUrl] = useState('')
+
+    return (
+        <div>
+            <h2>Create new</h2>
+            <form>
+                title: <input value={title} key="title" onChange={(event) => setTitle(event.target.value)} />
+                <p />
+                author: <input value={author} key="author" onChange={(event) => setAuthor(event.target.value)} />
+
+                <p />
+                url <input value={url} key="url" onChange={(event) => setUrl(event.target.value)} />
+                <p />
+                <button onClick={(event) => {
+                    createBlog(event, author, title, url)
+                    setTitle('')
+                    setAuthor('')
+                    setUrl('')
+                }}>create</button>
+            </form>
+        </div>
+    )
+}
+
+export default BlogForm
